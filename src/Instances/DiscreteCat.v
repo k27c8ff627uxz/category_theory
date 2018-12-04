ea8bb504e31e9a61d8d85954ca073e03915ada7f
MasahiroSato <k27c8ff627uxz@yahoo.co.jp>
2018-12-05 00:10:43 +0900

    Add Lemmas about 'Cat1'

--- src/Instances/DiscreteCat.v
+++ src/Instances/DiscreteCat.v
@@ -7,5 +7,34 @@ Program Definition DiscreteCat (A : Type) : Category :=
     Hom := fun x y => x = y;
   |}.
 
-Definition Cat1 := DiscreteCat True.
+Lemma DiscreteCat_Hom_eq :
+  forall {A : Type},
+  forall {X Y : Obj (DiscreteCat A)},
+  forall (f g : Hom X Y), f = g.
+Proof.
+  move => A X Y.
+  unfold Hom.
+  simpl.
+  move => f g.
+  exact: p_proof_irrelevance.
+Qed.
+
 Definition Cat0 := DiscreteCat False.
+
+Definition Cat1 := DiscreteCat True.
+Definition Cat1Obj : Obj Cat1 := I.
+
+Lemma Cat1Obj_eq :
+  forall (X Y : Obj Cat1), X = Y.
+Proof.
+  case.
+  case.
+  reflexivity.
+Qed.
+
+Lemma Cat1Hom_eq :
+  forall {X Y : Obj Cat1} (f g : Hom X Y), f = g.
+Proof.
+  move => X Y f g.
+  exact: DiscreteCat_Hom_eq.
+Qed.
